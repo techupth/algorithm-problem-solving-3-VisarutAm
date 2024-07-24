2f1bc93626e2383f68dc5b598e7c0a800499a487
Am Visarut <visarut.thk@gmail.com>
2024-07-24 15:01:33 +0700

    problem-solving

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,18 @@
 function sortCustomerName(customers) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let num = customers.length;
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num - 1 - i; j++) {
+      if (customers[j] > customers[j + 1]) {        
+        let customer = customers[j];
+        customers[j] = customers[j + 1];
+        customers[j + 1] = customer;
+      }
+    }
+  }
+  return customers;
 }
 
-// ตอบคำถามตรงนี้จ้า
+let customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"];
+console.log(sortCustomerName(customers)); 
+
+// Bubble Sort Algorithm มี Big O เป็น O(n^2) เพราะเป็น Loop 2ชั้น
